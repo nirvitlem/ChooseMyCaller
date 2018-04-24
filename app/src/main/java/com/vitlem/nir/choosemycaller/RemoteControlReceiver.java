d173ed75447534d8d1a189cddad6cb2b16037730
NirV <nvitlem@gmail.com>
2018-04-24 09:35:02 +0300

    24042018

--- app/src/main/java/com/vitlem/nir/choosemycaller/RemoteControlReceiver.java
+++ app/src/main/java/com/vitlem/nir/choosemycaller/RemoteControlReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.KeyEvent;
 
 public class RemoteControlReceiver extends BroadcastReceiver {
 
@@ -11,12 +12,20 @@ public class RemoteControlReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Log.d("RemoteControlReceiver ", intent.getAction());
-        if (intent.getAction().contains("VOLUME_CHANGED_ACTION")) {
 
-            TimingService.setcurrentvolume();
-            Log.i("VOLUME_CHANGED_ACTION ", String.valueOf(TimingService.volume));
-            ListLog.addtolist("VOLUME_CHANGED_ACTION " + String.valueOf(TimingService.volume) + " " + GetCurrentTime.GetTime());
+        Log.i("RemoteControlReceiver ", "RemoteControlReceiver");
+        if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+            KeyEvent event = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+            if (KeyEvent.KEYCODE_VOLUME_UP == event.getKeyCode()) {
+                TimingService.setcurrentvolume();
+                Log.i("KEYCODE_VOLUME_UP", "KEYCODE_VOLUME_UP");
+                ListLog.addtolist("KEYCODE_VOLUME_UP "  +String.valueOf(TimingService.volume) + " "+ GetCurrentTime.GetTime());
+            }
+            if (KeyEvent.KEYCODE_VOLUME_DOWN == event.getKeyCode()) {
+                TimingService.setcurrentvolume();
+                Log.i("KEYCODE_VOLUME_DOWN", "KEYCODE_VOLUME_DOWN");
+                ListLog.addtolist("KEYCODE_VOLUME_DOWN " +String.valueOf(TimingService.volume) + " "+ GetCurrentTime.GetTime());
+            }
         }
 
 
